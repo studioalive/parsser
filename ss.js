01a34066c8b098a636d64a6b5d62d15d53e879b2
studioalive <96373586+studioalive@users.noreply.github.com>
2022-10-15 14:22:18 +0100

    3 behind 4 in front

--- ss.js
+++ ss.js
@@ -1,19 +1,90 @@
 
 function parSSer() {
-    var ssInput = document.getElementById("ssText").value;   
-    const sentences = ssInput.split("."); 
+    document.getElementById("ssResult").innerHTML = "";
+    // ssInput = document.getElementById("ssText").value;  
+    ssInput = `Halliford School PE Shirt with school logo printed to left chest (TRANS-HALLIFORDWH), ss logo printed right chest (PRINT-SS-HALLIFORD), House name to reverse (PRINT-HALLIFORD) optional initials printed to right sleeve in white for £1.50 (40mm) and optional name tag printed inside the garment for £2.50`
+    sentences = ssInput.split(",");
     for (let i = 0; i < sentences.length; i++) {
-        const words = sentences[i].split(" ");
-        const sOut = "Sentence #" + i;
-        addText(sOut);
+        words = sentences[i].split(" ");
+        // const sOut = "Sentence #" + i;
+        // addText(sOut);
         for (let j = 0; j < words.length; j++) {
-        const wOut = "Word #" + j + ": " + words[j];
-        addText(wOut);        
+            wordCheck(words[j], j);
+            // const wOut = "Word #" + j + ": " + words[j];
+            // addText(wOut);        
         }
-      }
-    
-  }
+    }
+
+}
 
 function addText(txt) {
-    document.getElementById("ssResult").insertAdjacentHTML('beforeend',`<p>`+txt+`</p>`);
+    document.getElementById("ssResult").insertAdjacentHTML('beforeend', ` ` + txt + ` `);
+}
+
+function addBreak() {
+    document.getElementById("ssResult").insertAdjacentHTML('beforeend', `<br>`);
+}
+
+function wordCheck(word, pos) {
+    firstthree = word.substring(0, 3).toLowerCase();
+    console.log(firstthree);
+    switch (firstthree) {
+        case "cus":
+            custom(pos);
+            break;
+        case "ini":
+            initials(pos);
+            break;
+        case "ss":
+            ss(pos);
+            break;
+        case "bad":
+            badge(pos);
+            break;
+        case "cre":
+            badge(pos);
+            break;
+        case "log":
+            badge(pos);
+            break;
+        case "tri":
+            trim(pos);
+            break;
+        case "nam":
+            namefunc(pos);
+            break;
+    }
+}
+
+function wordScan(word, pos) {
+    addBreak();
+    for (let x = pos - 3; x < pos + 5; x++) {
+        if (typeof words[x] !== 'undefined') {
+            addText(words[x]);
+        }
+    }
+}
+
+function custom(pos) {
+
+}
+
+function badge(pos) {
+    wordScan("badge", pos);
+}
+
+function initials(pos) {
+    wordScan("initials", pos);
+}
+
+function ss(pos) {
+    wordScan("ss", pos);
+}
+
+function namefunc(pos) {
+    wordScan("name", pos);
+}
+
+function trim(pos) {
+    wordScan("trim", pos);
 }
